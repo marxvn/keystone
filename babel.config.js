fa2ffd0e4bc482bacf73fe0dd68740e2021203c9
Martin Pham <marxvn@gmail.com>
2019-12-13 09:16:59 +0700

    Fix typo

--- babel.config.js
+++ babel.config.js
@@ -19,7 +19,7 @@ module.exports = {
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
-    '@babel/proposal-object-rest-spread',
+    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
   ],
   overrides: [
